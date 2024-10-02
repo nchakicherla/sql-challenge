fca3b6171472c45c26483b362c7314da402e38d9
nchakicherla <nchaki6@gmail.com>
2024-10-02 00:34:58 -0700

    update script

--- EmployeeSQL/script.sql
+++ EmployeeSQL/script.sql
@@ -1,7 +1,3 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/lvdCt9
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
 DROP TABLE IF EXISTS "Titles" CASCADE;
 CREATE TABLE "Titles" (
     "title_id" varchar(8)   NOT NULL,
@@ -30,9 +26,6 @@ DROP TABLE IF EXISTS "Dept_Employee" CASCADE;
 CREATE TABLE "Dept_Employee" (
     "emp_no" int   NOT NULL,
     "dept_no" varchar(4)   NOT NULL
---    CONSTRAINT "pk_Dept_Employee" PRIMARY KEY (
---       "emp_no"
---     )
 );
 
 DROP TABLE IF EXISTS "Departments" CASCADE;
@@ -58,35 +51,52 @@ CREATE TABLE "Employees" (
      )
 );
 
+-- Add unique key constraints
+
 ALTER TABLE "Titles" ADD CONSTRAINT "unq_Titles_title_id" UNIQUE ("title_id");
 
 ALTER TABLE "Salaries" ADD CONSTRAINT "unq_Salaries_emp_no" UNIQUE ("emp_no");
 
 ALTER TABLE "Dept_Manager" ADD CONSTRAINT "unq_Dept_Manager_emp_no" UNIQUE ("emp_no");
 
---ALTER TABLE "Dept_Employee" ADD CONSTRAINT "unq_Dept_Employee_emp_no" UNIQUE ("emp_no");
-
 ALTER TABLE "Departments" ADD CONSTRAINT "unq_Departments_dept_no" UNIQUE ("dept_no");
 
 ALTER TABLE "Employees" ADD CONSTRAINT "unq_Employees_emp_no" UNIQUE ("emp_no");
 
--- ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
--- REFERENCES "Employees" ("emp_no");
+-- Add foreign key constraints
 
---ALTER TABLE "Dept_Manager" ADD CONSTRAINT "fk_Dept Manager_emp_no" FOREIGN KEY("emp_no")
---REFERENCES "Employees" ("emp_no");
+ALTER TABLE "Dept_Employee" ADD CONSTRAINT "fk_Dept_Employee_dept_no" FOREIGN KEY("dept_no")
+	REFERENCES "Departments" ("dept_no");
 
---ALTER TABLE "Dept_Employee" ADD CONSTRAINT "fk_Dept Employee_dept_no" FOREIGN KEY("dept_no")
---REFERENCES "Departments" ("dept_no");
+ALTER TABLE "Dept_Employee" ADD CONSTRAINT "fk_Dept_Employee_emp_no" FOREIGN KEY("emp_no")
+	REFERENCES "Employees" ("emp_no");
 
---ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_no" FOREIGN KEY("emp_no")
---REFERENCES "Dept_Employee" ("emp_no");
+ALTER TABLE "Dept_Manager" ADD CONSTRAINT "fk_Dept_Manager_dept_no" FOREIGN KEY("dept_no")
+	REFERENCES "Departments" ("dept_no");
 
---ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
---REFERENCES "Titles" ("title_id");
+ALTER TABLE "Dept_Manager" ADD CONSTRAINT "fk_Dept_Manager_emp_no" FOREIGN KEY("emp_no")
+	REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+	REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_no" FOREIGN KEY("emp_title_id")
+	REFERENCES "Titles" ("title_id");
+
+-- Import .csv data
 
 COPY "Titles" (title_id, title)
 FROM '/tmp/titles.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY "Employees" (emp_no, emp_title_id, birth_date, first_name, last_name, sex, hire_date)
+FROM '/tmp/employees.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY "Departments" (dept_no, dept_name)
+FROM '/tmp/departments.csv'
 DELIMITER ','
 CSV HEADER;
 
@@ -105,12 +115,77 @@ FROM '/tmp/dept_emp.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY "Departments" (dept_no, dept_name)
-FROM '/tmp/departments.csv'
-DELIMITER ','
-CSV HEADER;
+-- Analysis
+--
 
-COPY "Employees" (emp_no, emp_title_id, birth_date, first_name, last_name, sex, hire_date)
-FROM '/tmp/employees.csv'
-DELIMITER ','
-CSV HEADER;
+-- List the employee number, last name, first name, sex, and salary of each employee
+SELECT 
+	"Employees".emp_no, 
+	"Employees".first_name, 
+	"Employees".last_name,
+	"Employees".sex,
+	"Salaries".salary
+FROM 
+	"Employees"
+INNER JOIN
+	"Salaries"
+ON
+	"Salaries".emp_no = "Employees".emp_no;
+
+-- List the first name, last name, and hire date for the employees who were hired in 1986
+SELECT
+	"Employees".first_name,
+	"Employees".last_name,
+	"Employees".hire_date
+FROM
+	"Employees"
+WHERE
+	EXTRACT(YEAR FROM hire_date) = 1986
+;
+
+-- List the manager of each department along with their department number, department name, 
+-- employee number, last name, and first name
+SELECT
+	"Dept_Manager".dept_no,
+	"Departments".dept_name,
+	"Employees".emp_no,
+	"Employees".last_name,
+	"Employees".first_name
+FROM
+	"Dept_Manager"
+INNER JOIN
+	"Departments"
+ON
+	"Departments".dept_no = "Dept_Manager".dept_no
+INNER JOIN
+	"Employees"
+ON
+	"Employees".emp_no = "Dept_Manager".emp_no
+;
+
+
+-- List the department number for each employee along with that employee’s employee number, 
+-- last name, first name, and department name
+SELECT 
+	"Dept_Employee".dept_no,
+	"Employees".emp_no,
+	"Employees".last_name,
+	"Employees".first_name,
+	"Departments".dept_name
+FROM
+	"Employees"
+INNER JOIN
+	"Dept_Employee"
+ON
+	"Employees".emp_no = "Dept_Employee".emp_no
+INNER JOIN
+	"Departments"
+ON
+	"Dept_Employee".dept_no = "Departments".dept_no
+;
+
+-- List first name, last name, and sex of each employee whose first name is Hercules 
+-- and whose last name begins with the letter B
+SELECT 
+
+-- SELECT * FROM "Employees";
